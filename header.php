0553cc74fff4a2b26e9e2f080e813bdd910463f1
Owner <adamsnowden@users.noreply.github.com>
2017-03-12 23:56:57 +0000

    Update header.php " " to ' ' Support img
    Images are now supported after.change

--- header.php
+++ header.php
@@ -1,2 +1,2 @@
 <?php if(@fsockopen($_SERVER['REMOTE_ADDR'], 80, $errstr, $errno, 1))
-die("Mayhem's Handmade proxy blocker written in php "Place in your header trust me no more evading lol"); ?>
+die('Mayhem's Handmade proxy blocker written in php "Place in your header trust me no more evading lol'); ?>
